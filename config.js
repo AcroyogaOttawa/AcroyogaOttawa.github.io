e0f990bddb6a8afbc1e312dded362b85c09d9815
gregology <greg.clarke@jadedpixel.com>
2014-10-26 19:21:40 -0400

    using config file

---
+++ config.js
@@ -0,0 +1,15 @@
+/*!
+ * Acroyoga Community Calendar
+ * Original created for AcroyogaOttawa.com by Greg Clarke.
+ * Please share and forgive me for my horrile javascript skills :)
+ */
+
+var title = 'Acroyoga Ottawa Event Calendar';
+var tagline = 'Acrobatics with a yogic consciousness in a beautiful city';
+var instagram_hash_tag = 'AcroyogaOttawa';
+var instagram_client_id = '4c0caa35855143e788ca790012daaa3b';
+var google_calendar = 'gho.st_19b0g4d2m46ta2l2gf8olt1k6o@group.calendar.google.com';
+var social_links = '<a href="https://www.facebook.com/groups/AcroyogaOttawa/">Facebook</a> - <a href="https://twitter.com/AcroyogaOttawa">Twitter</a> - <a href="http://www.pinterest.com/gregology/acroyoga-ottawa/">Pinterest</a>';
+var contact_name = 'Greg';
+var contact_email = 'greg@smileyom.com';
+var google_analytics = 'UA-47043442-2';
