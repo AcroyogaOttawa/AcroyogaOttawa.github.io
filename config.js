39117653068cecdccdd41331e1ef391e7f94a454
gregology <greg.clarke@jadedpixel.com>
2014-11-21 23:04:20 -0500

    made changes for google api requirements

--- config.js
+++ config.js
@@ -8,6 +8,7 @@ var title = 'Acroyoga Ottawa Event Calendar';
 var tagline = 'Acrobatics with a yogic consciousness in a beautiful city';
 var instagram_hash_tag = 'AcroyogaOttawa';
 var instagram_client_id = '4c0caa35855143e788ca790012daaa3b';
+var google_calendar_api_key = 'AIzaSyAPjGPP7lWZPN1o1eBOeQzi7TL2jl3t5Ac';
 var google_calendar = 'gho.st_19b0g4d2m46ta2l2gf8olt1k6o@group.calendar.google.com';
 var social_links = '<a href="https://www.facebook.com/groups/AcroyogaOttawa/">Facebook</a> - <a href="https://twitter.com/AcroyogaOttawa">Twitter</a> - <a href="http://www.pinterest.com/gregology/acroyoga-ottawa/">Pinterest</a>';
 var contact_name = 'Greg';
